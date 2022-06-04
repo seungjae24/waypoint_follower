75e3d4a393bae5e387278963fca5670388190536
Seungjae Lee <sj98lee@gmail.com>
2022-06-04 22:36:48 +0900

    final

--- scripts/controller.py
+++ scripts/controller.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from distutils.log import error
+from threading import local
 import numpy as np
 import os
 import pandas as pd
@@ -14,6 +16,8 @@ from nav_msgs.msg import Odometry
 
 import tf
 from tf.transformations import euler_from_quaternion
+
+stanley = True
 
 # Calculate distance
 def calc_dist(tx, ty, ix, iy):
@@ -38,8 +42,9 @@ def global2local(ego_x, ego_y, ego_yaw, x_list, y_list):
         - ego_x, ego_y, ego_yaw        : ego vehicle's current pose.
         - output_x_list, output_y_list : transformed local coordinate trajectory.
     """
-    output_x_list = x_list
-    output_y_list = y_list
+
+    output_x_list =   math.cos(ego_yaw)*(x_list - ego_x) + math.sin(ego_yaw)*(y_list - ego_y)
+    output_y_list = - math.sin(ego_yaw)*(x_list - ego_x) + math.cos(ego_yaw)*(y_list - ego_y)
 
     return output_x_list, output_y_list
 
@@ -52,9 +57,13 @@ def find_nearest_point(ego_x, ego_y, x_list, y_list):
         - Use np.argmin for finding the index whose value is minimum.
     """
 
-    near_ind = -1
-    near_dist = 0
+    dist = []
+    for i in range(len(x_list)):
+        dist.append(calc_dist(ego_x, ego_y, x_list[i], y_list[i]))
 
+    near_ind = np.argmin(dist)
+    near_dist = dist[near_ind]
+        
     return near_dist, near_ind
 
 # Calculate Error
@@ -78,18 +87,33 @@ def calc_error(ego_x, ego_y, ego_yaw, x_list, y_list, wpt_look_ahead=0):
 
     # 2. Find the nearest waypoint
     _, near_ind = find_nearest_point(ego_x, ego_y, x_list, y_list)
-
+    
     # 3. Set lookahead waypoint (index of waypoint trajectory)
-    lookahead_wpt_ind = -1
-
+    lookahead_wpt_ind = (near_ind+wpt_look_ahead)%len(x_list)
+    
+    next_ind = (near_ind+1)%len(x_list)
     # 4. Calculate errors
-    error_yaw = 0
+    if stanley:
+
+        ego_x_front = ego_x + 0.25 * math.cos(ego_yaw)
+        ego_y_front = ego_y + 0.25 * math.sin(ego_yaw)
+        
+        _, near_ind_front = find_nearest_point(ego_x_front, ego_y_front, x_list, y_list)
+        next_front = (near_ind_front+1)%len(x_list)
+
+        error_yaw = math.atan2(local_y_list[next_front] - local_y_list[near_ind_front], local_x_list[next_front] - local_x_list[near_ind_front])
+        error_y = local_y_list[near_ind_front]
+
+        # error_yaw = math.atan2(local_y_list[lookahead_wpt_ind]-local_y_list[near_ind], local_x_list[lookahead_wpt_ind]-local_x_list[near_ind])
+        # error_yaw = math.atan2(local_y_list[next_ind] - local_y_list[near_ind], local_x_list[next_ind] - local_x_list[near_ind])
+    else:
+        error_yaw = math.atan2(local_y_list[lookahead_wpt_ind], local_x_list[lookahead_wpt_ind])
+        error_y   = local_y_list[lookahead_wpt_ind]
 
     error_yaw = normalize_angle(error_yaw) # Normalize angle to [-pi, +pi]
-    error_y   = 0
-    
-
-    return error_y, error_yaw
+    error_x   = local_x_list[lookahead_wpt_ind]
+        
+    return error_y, error_yaw, error_x
 
 class WaypointFollower():
     def __init__(self):
@@ -102,12 +126,22 @@ class WaypointFollower():
         self.MAX_STEER    = np.deg2rad(17.75)
 
         # vehicle state
-        self.ego_x   = 0
-        self.ego_y   = 0
-        self.ego_yaw = 0
-        self.ego_vx  = 0
+        self.ego_x   = 0.0
+        self.ego_y   = 0.0
+        self.ego_yaw = 0.0
+        self.ego_vx  = 0.0
 
-        self.wpt_look_ahead = 0   # [index]
+        # error prev, sum
+        self.error_yaw_sum = 0.0
+        self.error_yaw_prev = 0.0
+
+        self.error_y_sum = 0.0
+        self.error_y_prev = 0.0
+
+        self.error_v_sum = 0.0
+        self.error_v_prev = 0.0
+
+        self.wpt_look_ahead = 5   # [index]
 
         # Pub/Sub
         self.pub_command = rospy.Publisher('/control', AckermannDriveStamped, queue_size=5)
@@ -127,13 +161,37 @@ class WaypointFollower():
         _, _, self.ego_yaw = euler_from_quaternion(q_list)
 
     # Controller
-    def steer_control(self, error_y, error_yaw):
+    def steer_control(self, error_y, error_yaw, error_x):
         """
         TODO
         Implement a steering controller (PID controller or Pure pursuit or Stanley method).
         You can use not only error_y, error_yaw, but also other input arguments for this controller if you want.
         """
-        steer = 0
+
+        if stanley:
+            k_p_yaw = 0.001
+            k_i_yaw = 0.001
+            k_d_yaw = 0.001
+            
+            k_p_y = 1.0
+            k_i_y = 0.001
+            k_d_y = 0.001
+            
+            e_yaw = k_p_yaw * error_yaw + k_i_yaw * self.error_yaw_sum + k_d_yaw * (error_yaw - self.error_yaw_prev)
+            e_y   = k_p_y * error_y + k_i_y * self.error_y_sum + k_d_y * (error_y - self.error_y_prev)
+
+            k = 2.0
+            steer = e_yaw + math.atan2(k * e_y, self.ego_vx)
+
+            self.error_y_prev = error_y
+            self.error_y_sum = self.error_y_sum + error_y
+            self.error_yaw_prev = error_yaw
+            self.error_yaw_sum = self.error_yaw_sum + error_yaw
+            
+        else:
+            L = 0.05
+            l_d = math.sqrt(error_x**2 + error_y**2)
+            steer = math.atan2(2*L*math.sin(error_yaw), math.sqrt(l_d))
 
         # Control limit
         steer = np.clip(steer, -self.MAX_STEER, self.MAX_STEER)
@@ -146,8 +204,19 @@ class WaypointFollower():
         Implement a speed controller (PID controller).
         You can use not only error_v, but also other input arguments for this controller if you want.
         """
-        throttle = 0
+        k_p = 1.0
+        k_i = 0.001
+        k_d = 0.01
+
+        error_diff = error_v - self.error_v_prev
+
+        error_v_sum = self.error_v_sum + error_v
+
+        throttle = k_p * error_v + k_i * error_v_sum + k_d * error_diff
                 
+        self.error_v_prev = error_v
+        self.error_v_sum = error_v_sum
+
         return throttle
 
     def publish_command(self, steer, accel):
@@ -181,13 +250,13 @@ def main():
         ego_vx = wpt_control.ego_vx
 
         # Lateral error calculation (cross-track error, yaw error)
-        error_y, error_yaw = calc_error(ego_x, ego_y, ego_yaw, wpts_x, wpts_y, wpt_look_ahead=wpt_control.wpt_look_ahead)
+        error_y, error_yaw, error_x = calc_error(ego_x, ego_y, ego_yaw, wpts_x, wpts_y, wpt_look_ahead=wpt_control.wpt_look_ahead)
 
         # Longitudinal error calculation (speed error)
         error_v = wpt_control.target_speed - ego_vx
 
         # Control
-        steer_cmd = wpt_control.steer_control(error_y, error_yaw)
+        steer_cmd = wpt_control.steer_control(error_y, error_yaw, error_x)
         throttle_cmd = wpt_control.speed_control(error_v)
 
         # Publish command
